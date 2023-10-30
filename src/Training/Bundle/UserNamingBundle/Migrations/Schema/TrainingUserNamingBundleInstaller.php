30fb2df09f994d82b717cddf7357671a147ccf1c
OleksandrProtsiuk <oprotsiuk@oroinc.com>
2023-10-30 18:26:38 +0200

    INTAP-1727: training_project - Updated Installer.

--- src/Training/Bundle/UserNamingBundle/Migrations/Schema/TrainingUserNamingBundleInstaller.php
+++ src/Training/Bundle/UserNamingBundle/Migrations/Schema/TrainingUserNamingBundleInstaller.php
@@ -3,6 +3,9 @@
 namespace Training\Bundle\UserNamingBundle\Migrations\Schema;
 
 use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
 use Oro\Bundle\MigrationBundle\Migration\Installation;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
@@ -10,14 +13,24 @@ use Oro\Bundle\MigrationBundle\Migration\QueryBag;
  * @SuppressWarnings(PHPMD.TooManyMethods)
  * @SuppressWarnings(PHPMD.ExcessiveClassLength)
  */
-class TrainingUserNamingBundleInstaller implements Installation
+class TrainingUserNamingBundleInstaller implements Installation, ExtendExtensionAwareInterface
 {
+    private ExtendExtension $extendExtension;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setExtendExtension(ExtendExtension $extendExtension): void
+    {
+        $this->extendExtension = $extendExtension;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function getMigrationVersion(): string
     {
-        return 'v1_0';
+        return 'v1_1';
     }
 
     /**
@@ -27,6 +40,9 @@ class TrainingUserNamingBundleInstaller implements Installation
     {
         /** Tables generation **/
         $this->createUnbUserNamingTypeTable($schema);
+
+        /** Foreign keys generation **/
+        $this->addRelationFromUser($schema);
     }
 
     /**
@@ -42,5 +58,27 @@ class TrainingUserNamingBundleInstaller implements Installation
         $table->addColumn('format', 'string', ['length' => 255]);
         $table->addColumn('serialized_data', 'json', ['notnull' => false]);
         $table->setPrimaryKey(['id']);
+    }
+
+    /**
+     * Adds many-to-one relation with User entity
+     *
+     * @param Schema $schema
+     * @return void
+     */
+    protected function addRelationFromUser(Schema $schema): void
+    {
+        $this->extendExtension->addManyToOneRelation(
+            $schema,
+            'oro_user',
+            'typeOfNaming',
+            'unb_user_naming_type',
+            'title',
+            [
+                'extend' => [
+                    'owner' => ExtendScope::OWNER_CUSTOM
+                ]
+            ]
+        );
     }
 }
